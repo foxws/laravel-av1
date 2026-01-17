81010abc5ff1647e772515103e7e3875749974db
francoism90 <francoism90@users.noreply.github.com>
2026-01-17 22:23:54 +0100

    Update PHP version in CI workflow to 8.3 and add ab-av1 setup step in tests

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -4,4 +4,5 @@ declare(strict_types=1);
 
 arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
-    ->not->toBeUsed();
+    ->not->toBeUsed()
+    ->ignoring('Foxws\\AV1\\Exporters\\MediaExporter');
