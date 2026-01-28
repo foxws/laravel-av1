1efe04359062c4ac2181d8ecccfc72f937ce5ac4
francoism90 <francoism90@users.noreply.github.com>
2026-01-28 12:26:27 +0100

    feat: Update AV1 facade methods and change facade accessor to AV1Manager

--- src/Facades/AV1.php
+++ src/Facades/AV1.php
@@ -7,29 +7,16 @@ namespace Foxws\AV1\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Foxws\AV1\MediaOpener open($paths)
- * @method static \Foxws\AV1\MediaOpener fromDisk(\Foxws\AV1\Filesystem\Disk|\Illuminate\Contracts\Filesystem\Filesystem|string $disk)
- * @method static \Foxws\AV1\MediaOpener vmafEncode()
- * @method static \Foxws\AV1\MediaOpener crfSearch()
- * @method static \Foxws\AV1\MediaOpener sampleEncode()
- * @method static \Foxws\AV1\MediaOpener encode()
- * @method static \Foxws\AV1\MediaOpener vmaf()
- * @method static \Foxws\AV1\MediaOpener xpsnr()
- * @method static \Foxws\AV1\MediaOpener input(string $path)
- * @method static \Foxws\AV1\MediaOpener output(string $path)
- * @method static \Foxws\AV1\MediaOpener preset(string $preset)
- * @method static \Foxws\AV1\MediaOpener minVmaf(float|int $vmaf)
- * @method static \Foxws\AV1\MediaOpener crf(int $crf)
- * @method static \Foxws\AV1\MediaOpener reference(string $path)
- * @method static \Foxws\AV1\MediaOpener distorted(string $path)
- * @method static \Foxws\AV1\Exporters\MediaExporter export()
+ * @method static int findCrf(string $inputPath, float|int $targetVmaf = 95, int $preset = 6, ?int $minCrf = null, ?int $maxCrf = null)
+ * @method static \Foxws\AV1\FFmpeg\VideoEncoder encoder()
+ * @method static \Foxws\AV1\AbAV1\CrfFinder crfFinder()
  *
- * @see \Foxws\AV1\MediaOpener
+ * @see \Foxws\AV1\AV1Manager
  */
 class AV1 extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return \Foxws\AV1\MediaOpener::class;
+        return \Foxws\AV1\AV1Manager::class;
     }
 }
